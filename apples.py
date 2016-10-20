d4792aa699516cb8a39c2f50b995c27395229c5c
valik-pc <valik.v1per@gmail.com>
2016-10-20 20:19:18 +0300

    Implement second and third tasks

---
+++ apples.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.5
+
+print("Входные данные")
+
+number1 = int(input())
+number2 = int(input())
+
+print("Выходые данные")
+print(number2 // number1)
+print(number2 % number1)
---
+++ triangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.5
+
+print("Входные данные")
+
+number1 = int(input())
+number2 = int(input())
+
+print("Выходые данные")
+print(0.5 * number1 * number2)
