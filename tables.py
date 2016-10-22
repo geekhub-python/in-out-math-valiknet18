b00c0bc9428d20a6eb7ce9f3ae1956328dd62580
valik-pc <valik.v1per@gmail.com>
2016-10-22 15:32:19 +0300

    Implement tables task

---
+++ tables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.5
+
+print("Входные данные")
+class1 = round(int(input()) / 2 + 0.1)
+class2 = round(int(input()) / 2 + 0.1)
+class3 = round(int(input()) / 2 + 0.1)
+
+print("Выходные данные")
+print(class1 + class2 + class3)
+
